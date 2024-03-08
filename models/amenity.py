f5b75e119545c1d598b3534f7c5e2a239c77f24a
Esegbe-Able <esegbeable6@gmail.com>
2024-03-08 12:15:05 +0000

    Files Commit

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Model creates a class called amenity"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class for amenity objects managements"""
+
+    name = ""
